6484372e023aaf068f3a479a95375854ad56def4
hgb926 <hgb926@naver.com>
2024-07-20 15:57:59 +0900

    reportWebVitals 삭제, 로그인 컴포넌트 생성

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
+import RootLayout from "../layout/RootLayout";
+import Home from "../pages/Home";
+import WelcomePage from "../pages/WelcomePage";
+import SignUpPage from "../pages/SignUpPage";
+
+
+
+const homeRouter = [
+  {
+    index: true,
+    element: <WelcomePage/>,
+  },
+  {
+    path: 'sign-up',
+    element: <SignUpPage />
+  },
+]
 
 export const router= createBrowserRouter([
   {
-
+    path: '/',
+    element: <RootLayout/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>,
+        children: homeRouter,
+      },
+    ]
   }
 ]);
+
