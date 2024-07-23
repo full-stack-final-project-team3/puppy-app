1f2ffcd1fb5be525a2057ef42797819491e1c792
zinit95 <kyj32323@naver.com>
2024-07-23 15:10:17 +0900

    shop 폴더 맨들고 리뷰페이지 만듬

--- src/config/user/route-config.js
+++ src/config/user/route-config.js
@@ -9,6 +9,7 @@ import HotelPage from '../../pages/hotel/HotelPage'; // 새로 추가된 HotelPa
 import AddHotelPage from '../../pages/hotel/AddHotelPage'; // 새로 추가된 AddHotelPage
 import { userDataLoader, authCheckLoader } from './auth';
 import { logoutAction } from '../../pages/user/Logout';
+import ReviewPage from '../../pages/shop/ReviewPage';
 
 const homeRouter = [
   {
@@ -51,6 +52,11 @@ export const router = createBrowserRouter([
         path: 'add-hotel',
         element: <AddHotelPage />,
         loader: authCheckLoader, // 로그인 정보를 확인하는 loader 추가
+      },
+      {
+        path: 'review-page',
+        element: <ReviewPage />,
+        loader: authCheckLoader, // 리뷰페이지 
       },
     ],
   },
