2df5127bb493edf7e01d229f1bbbb9506ca6c4dd
hgb926 <hgb926@naver.com>
2024-07-23 14:16:06 +0900

    0723 LoginForm 왼쪽 만듬

--- src/components/auth/LoginForm.js
+++ src/components/auth/LoginForm.js
@@ -2,31 +2,18 @@ import React from 'react';
 import { Form, Link, redirect } from 'react-router-dom';
 import styles from './LoginForm.module.scss';
 import { AUTH_URL } from "../../config/host-config";
+import LoginFormLeft from "./LoginFormLeft";
+import LoginFormRight from "./LoginFormRight";
 
 const LoginForm = () => {
     return (
         <div className={styles.authContainer}>
-            <Form method="post">
-                <h1>Log in</h1>
-                <p>
-                    <label htmlFor="email">Email</label>
-                    <input id="email" type="email" name="email" required />
-                </p>
-                <p>
-                    <label htmlFor="password">Password</label>
-                    <input id="password" type="password" name="password" required />
-                </p>
-                <div className={styles.checkboxContainer}>
-                    <input type="checkbox" id="autoLogin" name="isAutoLogin" />
-                    <label htmlFor="autoLogin">자동 로그인</label>
-                </div>
-                <div>
-                    <button type="submit">Login</button>
-                </div>
-                <div className={styles.signupLink}>
-                    <Link to="/signup">회원가입</Link>
-                </div>
-            </Form>
+            <div>
+                <LoginFormLeft />
+            </div>
+            <div>
+                <LoginFormRight />
+            </div>
         </div>
     );
 };
---
+++ src/components/auth/LoginFormRight.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoginFormRight = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default LoginFormRight;
