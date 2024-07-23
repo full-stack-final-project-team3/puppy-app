becb56c4ca0842f98d46424e151434e17e10e16f
Jehoon <limjhoon8@gmail.com>
2024-07-23 12:49:27 +0900

    ✅ 헤더 업데이트

--- src/layout/MainNavigation.js
+++ src/layout/MainNavigation.js
@@ -1,37 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
 import styles from './MainNavigation.module.scss';
-import { NavLink, useRouteLoaderData } from 'react-router-dom';
 import { GiHamburgerMenu } from "react-icons/gi";
-import {BsBell} from "react-icons/bs";
-import {BiUser} from "react-icons/bi";
+import { BsBell } from "react-icons/bs";
+import { BiUser } from "react-icons/bi";
 
 const MainNavigation = () => {
-    const userData = useRouteLoaderData('user-data');
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const storedUser = localStorage.getItem('userData');
+        if (storedUser) {
+            setIsLoggedIn(true);
+        }
+    }, []);
+
+    const toggleMenuHandler = () => {
+        setMenuOpen(prevState => !prevState);
+    };
+
+    const logoutHandler = () => {
+        localStorage.removeItem('userData');
+        setIsLoggedIn(false);
+        window.location.reload(); // 로그아웃 후 페이지 새로고침
+    };
 
     return (
         <header className={styles.header}>
             <nav className={styles.nav}>
                 <div className={styles.left}></div>
                 <div className={styles.center}>
-                    <NavLink to='/login'>
+                    <NavLink to='/'>
                         <img src="/header-logo.png" alt="Header Logo" />
                     </NavLink>
                 </div>
                 <div className={styles.right}>
-                    { !userData ? (
+                    {isLoggedIn ? (
                         <>
-                            <NavLink className={styles.login} to='/login'>Login</NavLink>
-                            <GiHamburgerMenu className={styles.icon} />
+                            <button className={styles.logout} onClick={logoutHandler}>Logout</button>
+                            <BsBell className={styles.icon} />
+                            <BiUser className={styles.icon} /> 
+                            <GiHamburgerMenu className={styles.icon} onClick={toggleMenuHandler} />
                         </>
                     ) : (
                         <>
-                            <BsBell className={styles.icon}/>
-                            <BiUser className={styles.icon}/>
-                            <GiHamburgerMenu className={styles.icon}/>
+                            <NavLink className={styles.login} to='/login'>Login</NavLink>
+                            <BiUser className={styles.icon} /> {/* 여기에 추가 */}
+                            <GiHamburgerMenu className={styles.icon} onClick={toggleMenuHandler} />
                         </>
                     )}
                 </div>
             </nav>
+            {menuOpen && (
+                <div className={styles.dropdownMenu}>
+                    <ul>
+                        <li><NavLink to='/category1'>Category 1</NavLink></li>
+                        <li><NavLink to='/category2'>Category 2</NavLink></li>
+                        <li><NavLink to='/category3'>Category 3</NavLink></li>
+                    </ul>
+                </div>
+            )}
         </header>
     );
 }
