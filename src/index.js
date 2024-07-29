47211f1c41c64b8be5c65aad0e682a0e38353170
Jehoon <limjhoon8@gmail.com>
2024-07-30 08:33:41 +0900

    🐛 호텔 슬라이드 라이브러리 설치.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import store from './components/store/index';
 import {Provider} from 'react-redux'
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
