279c9ae80db17ce48b06b5b6a9548619ea23d80e
kkimjuseong <rofodzl1@naver.com>
2024-08-04 15:25:27 +0900

    # 예약하기 기능 완료

--- src/components/hotel/HotelConfirmation.js
+++ src/components/hotel/HotelConfirmation.js
@@ -1,54 +1,81 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import styles from './HotelConfirmation.module.scss';
+import { useNavigate } from 'react-router-dom';
+import { submitReservation } from '../store/hotel/HotelReservationSlice';
 
 const HotelConfirmation = ({ hotel, selectedRoom, startDate, endDate, totalPrice, user }) => {
-  const personCount = useSelector(state => state.hotelPage.personCount);
-  console.log('user: ', user);
-  const formatDate = (dateStr) => {
-    const date = new Date(dateStr);
-    const options = { weekday: 'short', year: 'numeric', month: '2-digit', day: '2-digit' };
-    return date.toLocaleDateString('en-US', options).replace(',', '').replace(/\//g, ' / ');
-  };
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const personCount = useSelector(state => state.hotelPage.personCount);
+    console.log('user: ', user);
 
-  return (
-    <div className={styles.bookingConfirmation}>
-      <h2>Booking Confirmation</h2>
-      <p>Hotel: {hotel['hotel-name']}</p>
-      <p>Room: {selectedRoom.name}</p>
-      <div className={styles.priceDetails}>
-        <span className={styles.priceLabel}>Total Price: </span>
-        <span className={styles.priceValue}>{totalPrice}</span>
-      </div>
-      <div className={styles.personCount}>
-        <span>Person Count: {personCount}</span>
-      </div>
-      <div className={styles.dateDetails}>
-        <p>Check-in Date: {formatDate(startDate)}</p>
-        <p>Check-out Date: {formatDate(endDate)}</p>
-      </div>
-      <div className={styles.userInfo}>
-        <p>User: {user.realName}</p> {/* Assuming user object has a name property */}
-      </div>
-      <button>Confirm Booking</button>
-    </div>
-  );
+    const handleConfirmBooking = () => {
+        dispatch(submitReservation({
+            hotelId: hotel['hotel-id'],
+            roomId: selectedRoom['room-id'],
+            startDate,
+            endDate,
+            userId: user.userId,
+            totalPrice
+        }))
+            .unwrap()
+            .then((response) => {
+                console.log('Reservation succeeded:', response);
+                // 성공 시 예약 완료 페이지로 이동
+                navigate('/hotel', { state: { reservation: response } });
+                alert("예약이 완료되었습니다.")
+            })
+            .catch((error) => {
+                console.error('Reservation failed:', error);
+                alert("예약이 되어있는 객실입니다.")
+            });
+    };
+
+    const formatDate = (dateStr) => {
+        const date = new Date(dateStr);
+        const options = { weekday: 'short', year: 'numeric', month: '2-digit', day: '2-digit' };
+        return date.toLocaleDateString('en-US', options).replace(',', '').replace(/\//g, ' / ');
+    };
+
+    return (
+        <div className={styles.bookingConfirmation}>
+            <h2>Booking Confirmation</h2>
+            <p>Hotel: {hotel['hotel-name']}</p>
+            <p>Room: {selectedRoom.name}</p>
+            <div className={styles.priceDetails}>
+                <span className={styles.priceLabel}>Total Price: </span>
+                <span className={styles.priceValue}>{totalPrice}</span>
+            </div>
+            <div className={styles.personCount}>
+                <span>Person Count: {personCount}</span>
+            </div>
+            <div className={styles.dateDetails}>
+                <p>Check-in Date: {formatDate(startDate)}</p>
+                <p>Check-out Date: {formatDate(endDate)}</p>
+            </div>
+            <div className={styles.userInfo}>
+                <p>User: {user.realName}</p> {/* Assuming user object has a realName property */}
+            </div>
+            <button onClick={handleConfirmBooking}>Confirm Booking</button>
+        </div>
+    );
 };
 
 HotelConfirmation.propTypes = {
-  hotel: PropTypes.shape({
-    'hotel-name': PropTypes.string.isRequired,
-  }).isRequired,
-  selectedRoom: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-  }).isRequired,
-  startDate: PropTypes.string.isRequired,
-  endDate: PropTypes.string.isRequired,
-  totalPrice: PropTypes.number.isRequired,
-  user: PropTypes.shape({
-    name: PropTypes.string.isRequired, // Adjust according to actual user properties
-  }).isRequired,
+    hotel: PropTypes.shape({
+        'hotel-name': PropTypes.string.isRequired,
+    }).isRequired,
+    selectedRoom: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+    }).isRequired,
+    startDate: PropTypes.string.isRequired,
+    endDate: PropTypes.string.isRequired,
+    totalPrice: PropTypes.number.isRequired,
+    user: PropTypes.shape({
+        realName: PropTypes.string.isRequired, // Adjust according to actual user properties
+    }).isRequired,
 };
 
 export default HotelConfirmation;
