517d295b83fac6e6e978b07a9efc7c06e8e35154
hgb926 <hgb926@naver.com>
2024-08-04 21:30:27 +0900

    0804 유저 프사 업뎃 가능, 강아지는 아직

--- src/components/auth/dog/AddDogMain.js
+++ src/components/auth/dog/AddDogMain.js
@@ -1,186 +1,159 @@
-import React, { useState } from 'react';
-import DogNameInput from "./DogNameInput";
-import DogBirthdayInput from "./DogBirthdayInput";
-import DogBreedInput from "./DogBreedInput";
-import styles from "./AddDogMain.module.scss";
-import DogSexInput from "./DogSexInput";
-import DogWeightInput from "./DogWeightInput";
-import DogAllergiesInput from "./DogAllergiesInput";
-import { useNavigate } from "react-router-dom";
-import { DOG_URL, NOTICE_URL } from "../../../config/user/host-config";
+import React, { useRef, useState } from 'react';
+import styles from './DogEdit.module.scss';
 import { useDispatch, useSelector } from "react-redux";
 import { userEditActions } from "../../store/user/UserEditSlice";
+import { dogEditActions } from "../../store/dog/DogEditSlice";
+import { DOG_URL } from "../../../config/user/host-config";
+import { useNavigate } from "react-router-dom";
+import { decideGender, decideSize, translateAllergy, translateBreed } from './dogUtil.js';
+import { LuBadgeX } from "react-icons/lu";
 
-const AddDogMain = () => {
-    const navigate = useNavigate();
-    const user = useSelector(state => state.userEdit.userDetail);
+const DogEdit = () => {
+    const [isModifyMode, setIsModifyMode] = useState(false);
+    const weightRef = useRef();
+    const fileInputRef = useRef(); // 파일 입력에 대한 참조 추가
+
+    const dog = useSelector(state => state.dogEdit.dogInfo);
+    const userDetail = useSelector(state => state.userEdit.userDetail);
+    const navi = useNavigate();
     const dispatch = useDispatch();
-    const email = user.email;
 
-    // 갔다온 적이 있나? 로 막아버리기
-    const [visitedSteps, setVisitedSteps] = useState([1]);
+    const clearEditMode = async e => {
+        dispatch(userEditActions.clearMode());
+        dispatch(dogEditActions.clearEdit());
+        const weight = weightRef.current.value;
 
+        const payload = { weight };
 
-    const [step, setStep] = useState(1);
-    const [name, setName] = useState('');
-    const [breed, setBreed] = useState('');
-    const [birthday, setBirthday] = useState(null);
-    const [gender, setGender] = useState('');
-    const [weight, setWeight] = useState('');
-    const [dogSize, setDogSize] = useState('');
-    const [allergies, setAllergies] = useState([]);
-    const profileUrl = "https://search.pstatic.net/sunny/?src=https%3A%2F%2Fst2.depositphotos.com%2F5045705%2F11671%2Fv%2F950%2Fdepositphotos_116714982-stock-illustration-little-puppy-icon.jpg&type=a340";
-
-
-    const dogNameValue = (dogName) => {
-        setName(dogName);
-        setStep(2);
-        setVisitedSteps([1,2])
-    };
-
-    const dogBreedValue = (dogBreed) => {
-        setBreed(dogBreed);
-        setStep(3);
-        setVisitedSteps([1,2,3])
-    };
-
-    const dogBirthdayValue = (date) => {
-        const formatDate = date.format('YYYY-MM-DD');
-        setBirthday(formatDate);
-        setStep(4);
-        setVisitedSteps([1,2,3,4])
-    };
-
-    const dogSexValue = (sex) => {
-        setGender(sex);
-        setStep(5);
-        setVisitedSteps([1,2,3,4,5])
-    }
-
-    const dogWeightValue = (weight) => {
-        setWeight(weight);
-        if (weight > 25) {
-            setDogSize("LARGE");
-        } else if (weight > 10) {
-            setDogSize("MEDIUM");
-        } else {
-            setDogSize("SMALL");
+        const response = await fetch(`${DOG_URL}/${dog.id}`, {
+            method: "PATCH",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(payload),
+        });
+        if (response.ok) {
+            alert("수정 되었습니다.");
         }
-        setStep(6);
-    }
+    };
 
-    const dogAllergiesValue = async (allergies) => {
-        setAllergies(allergies);
-
-        const payload = {
-            dogName: name,
-            dogBreed: breed,
-            birthday: birthday,
-            dogSex: gender,
-            dogSize: dogSize,
-            weight: weight,
-            allergies: allergies,
-            dogProfileUrl: profileUrl,
-        };
-
+    const removeHandler = async () => {
         try {
-            const response = await fetch(`${DOG_URL}/register/${email}`, {
-                method: 'POST',
+            const response = await fetch(`${DOG_URL}/${dog.id}`, {
+                method: 'DELETE',
                 headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(payload)
             });
 
-            const newDog = await response.json();
-
             if (response.ok) {
-                const updatedUserDetail = {
-                    ...user,
-                    dogList: [...user.dogList, newDog]
-                };
-
-                dispatch(userEditActions.updateUserDetail(updatedUserDetail));
-
-                const noticePayload = {
-                    userId: user.id,
-                    message: `${name}가(이) 등록되었습니다!`
-                };
-
-                try {
-                    const noticeResponse = await fetch(`${NOTICE_URL}/add`, {
-                        method: 'POST',
-                        headers: { 'Content-Type': 'application/json' },
-                        body: JSON.stringify(noticePayload)
-                    });
-
-                    const noticeResponseText = await noticeResponse.text();
-
-                    console.log(noticeResponseText);
-
-                    let newNotice;
-                    try {
-                        newNotice = JSON.parse(noticeResponseText);
-                    } catch (jsonError) {
-                        alert('강아지 등록은 성공했으나 알림 추가에 실패했습니다.');
-                        return;
-                    }
-
-                    if (noticeResponse.ok) {
-                        dispatch(userEditActions.addUserNotice(newNotice));
-                        const updatedUserDetailWithNoticeCount = {
-                            ...user,
-                            noticeCount: user.noticeCount + 1
-                        };
-                        dispatch(userEditActions.updateUserDetail(updatedUserDetailWithNoticeCount));
-                        alert('강아지 등록 성공!');
-                    } else {
-                        alert('강아지 등록은 성공했으나 알림 추가에 실패했습니다.');
-                    }
-
-                } catch (error) {
-                    alert('강아지 등록은 성공했으나 알림 추가 중 오류가 발생했습니다.');
-                }
-
-                // navigate 호출을 상태 업데이트 후에 실행
-                navigate('/mypage', { state: { newDog }, replace: true });
+                alert("삭제 되었습니다.");
+                const updatedDogList = userDetail.dogList.filter(d => d.id !== dog.id);
+                dispatch(userEditActions.updateUserDetail({ ...userDetail, dogList: updatedDogList }));
+                dispatch(userEditActions.clearMode());
+                dispatch(dogEditActions.clearEdit());
+                navi("/mypage");
             } else {
-                alert('강아지 등록 실패!');
+                alert("삭제에 실패했습니다.");
+                console.error('삭제 실패:', response.statusText);
             }
         } catch (error) {
-            alert('강아지 등록 중 오류가 발생했습니다.');
+            console.error('에러 발생:', error);
+            alert("삭제 중 에러가 발생했습니다.");
         }
-    }
+    };
 
-    const switchStep = (num) => {
-        if (visitedSteps.length >= num) setStep(num)
-    }
+    const removeAllergy = async e => {
+        const allergy = e.target.dataset.alg;
+
+        try {
+            const response = await fetch(`${DOG_URL}/allergy?allergy=${allergy}&dogId=${dog.id}`, {
+                method: 'DELETE',
+                headers: { 'Content-Type': 'application/json' },
+            });
+
+            if (response.ok) {
+                const updatedAllergies = dog.allergies.filter(a => a !== allergy);
+                const updatedDogList = userDetail.dogList.map(d =>
+                    d.id === dog.id ? { ...d, allergies: updatedAllergies } : d
+                );
+
+                dispatch(userEditActions.updateUserDetail({ ...userDetail, dogList: updatedDogList }));
+                dispatch(dogEditActions.updateDogInfo({ ...dog, allergies: updatedAllergies }));
+                alert("강아지의 건강이 좋아졌나봐요!");
+            } else {
+                console.error('알러지 삭제 실패:', response.statusText);
+            }
+        } catch (error) {
+            console.error('알러지 삭제 중 에러 발생:', error);
+        }
+    };
+
+    const handleImageClick = () => {
+        fileInputRef.current.click(); // 파일 입력을 클릭하도록 트리거
+    };
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                dispatch(dogEditActions.updateDogInfo({ ...dog, dogProfileUrl: reader.result }));
+            };
+            reader.readAsDataURL(file);
+        }
+    };
 
     return (
-        <>
-            <div className={styles.stepWrap}>
-                <div className={styles.subWrap}>
-                    <div className={`${styles.step} ${step >= 1 ? styles.finishStep : ''}`} onClick={() => switchStep(1)}>1</div>
-                    <div className={`${styles.stepLine} ${step >= 2 ? styles.finishStep : ''}`}></div>
-                    <div className={`${styles.step} ${step >= 2 ? styles.finishStep : ''}`} onClick={() => switchStep(2)}>2</div>
-                    <div className={`${styles.stepLine} ${step >= 3 ? styles.finishStep : ''}`}></div>
-                    <div className={`${styles.step} ${step >= 3 ? styles.finishStep : ''}`} onClick={() => switchStep(3)}>3</div>
-                    <div className={`${styles.stepLine} ${step >= 4 ? styles.finishStep : ''}`}></div>
-                    <div className={`${styles.step} ${step >= 4 ? styles.finishStep : ''}`} onClick={() => switchStep(4)}>4</div>
-                    <div className={`${styles.stepLine} ${step >= 5 ? styles.finishStep : ''}`}></div>
-                    <div className={`${styles.step} ${step >= 5 ? styles.finishStep : ''}`} onClick={() => switchStep(5)}>5</div>
-                    <div className={`${styles.stepLine} ${step >= 6 ? styles.finishStep : ''}`}></div>
-                    <div className={`${styles.step} ${step >= 6 ? styles.finishStep : ''}`} onClick={() => switchStep(6)}>6</div>
+        <div className={styles.wrap}>
+            <div className={styles.header}>
+                <img className={styles.img} src="/header-logo.png" alt="Header Logo" />
+                <h2 className={styles.title}>{dog.dogName}의 정보</h2>
+            </div>
+            <div className={styles.flex}>
+                <div className={styles.left}>
+                    <div className={styles.row}>
+                        <div className={styles.item}>견종</div>
+                        <span className={styles.answer}>{translateBreed(dog.dogBreed)}</span>
+                    </div>
+                    <div className={styles.row}>
+                        <div className={styles.item}>생일</div>
+                        <span className={styles.answer}>{dog.birthday} <span className={styles.sub}>{dog.age}년 {dog.month}개월</span></span>
+                    </div>
+                    <div className={styles.row}>
+                        <div className={styles.item}>성별</div>
+                        <span className={styles.answer}>{decideGender(dog.dogSex)}</span>
+                    </div>
+                    <div className={styles.row}>
+                        <div className={styles.item}>체중</div>
+                        <input className={styles.input} placeholder={dog.weight} type="number" ref={weightRef} />
+                        {!isModifyMode ? <span className={styles.sub}>{decideSize(dog.dogSize)}</span> : <button>완료</button>}
+                    </div>
+                    <div className={styles.row}>
+                        <div className={styles.item}>보유 알러지</div>
+                        <div className={styles.answer}>
+                            {Array.isArray(dog.allergies) ? dog.allergies.map((allergy, index) => (
+                                <span key={index} className={styles.badge}>
+                                    {translateAllergy(allergy)}
+                                    <LuBadgeX className={styles.icon} onClick={removeAllergy} data-alg={allergy} />
+                                </span>
+                            )) : <span className={styles.answer}>없음</span>}
+                        </div>
+                    </div>
+                    <div className={styles.buttons}>
+                        <button onClick={removeHandler}>삭제</button>
+                        <button onClick={clearEditMode}>완료</button>
+                    </div>
+                </div>
+                <div className={styles.right} onClick={handleImageClick}>
+                    <img className={styles.dogProfileUrl} src={dog.dogProfileUrl} alt="Dog Profile" />
+                    <div className={styles.hoverText}>강아지 사진 변경</div>
+                    <input
+                        type="file"
+                        ref={fileInputRef}
+                        style={{ display: 'none' }}
+                        onChange={handleFileChange}
+                    />
                 </div>
             </div>
-            <div className={styles.wrap}>
-                {step === 1 && <DogNameInput dogNameValue={dogNameValue}/>}
-                {step === 2 && <DogBreedInput dogBreedValue={dogBreedValue}/>}
-                {step === 3 && <DogBirthdayInput onDateChange={dogBirthdayValue}/>}
-                {step === 4 && <DogSexInput dogSexValue={dogSexValue}/>}
-                {step === 5 && <DogWeightInput dogWeightValue={dogWeightValue}/>}
-                {step === 6 && <DogAllergiesInput onAllergiesChange={dogAllergiesValue}/>}
-            </div>
-        </>
+        </div>
     );
 };
 
-export default AddDogMain;
+export default DogEdit;
--- src/pages/shop/review/WriteReviewPage.js
+++ src/pages/shop/review/WriteReviewPage.js
@@ -1,18 +1,19 @@
-// src/components/Review/WriteReviewPage.js
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import RatingInput from './RatingInput';
 import styles from './Review.module.scss';
+import { userEditActions } from "../../../components/store/user/UserEditSlice";
+import { NOTICE_URL } from "../../../config/user/host-config"
 
 const WriteReviewPage = () => {
   const [reviewContent, setReviewContent] = useState('');
   const [rate, setRate] = useState(5);
   const navigate = useNavigate();
   const user = useSelector((state) => state.userEdit.userDetail);
+  const dispatch = useDispatch();
 
-
-  const treatsId = 'b1c2d3e4-f5g6-7890-ab12-c3d4e5f67891';   // 실제 treatsId 값
+  const treatsId = 'b1c2d3e4-f5g6-7890-ab12-c3d4e5f67891'; // 실제 treatsId 값
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -50,35 +51,67 @@ const WriteReviewPage = () => {
       }
 
       console.log('리뷰 제출 성공');
-      navigate('/review-page');
+      const noticePayload = {
+        userId: user.id,
+        message: `리뷰가 성공적으로 등록되었습니다.`
+      };
+
+      try {
+        const noticeResponse = await fetch(`${NOTICE_URL}/add`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(noticePayload)
+        });
+
+        if (!noticeResponse.ok) {
+          const noticeErrorText = await noticeResponse.text();
+          console.error('알림 등록 실패:', noticeErrorText);
+          throw new Error('알림 등록 실패');
+        }
+
+        const noticeResponseText = await noticeResponse.text();
+        console.log(noticeResponseText);
+
+        const newNotice = JSON.parse(noticeResponseText);
+        dispatch(userEditActions.addUserNotice(newNotice));
+        const updatedUserDetailWithNoticeCount = {
+          ...user,
+          noticeCount: user.noticeCount + 1
+        };
+        dispatch(userEditActions.updateUserDetail(updatedUserDetailWithNoticeCount));
+
+        navigate('/review-page');
+      } catch (error) {
+        console.error('알림 등록 오류:', error);
+      }
     } catch (error) {
       console.error('리뷰 제출 오류:', error);
     }
   };
 
   return (
-    <div className={`${styles.review_common_box} ${styles.review_writer_box}`}>
-      <h1>리뷰 작성하기</h1>
-      <p> 닉네임 : {user.nickname} </p> 
-      <p> 이메일 : {user.email} </p> 
-      <p> Treats ID : {treatsId} </p> {/* 실제 Treats ID 값 표시 */}
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="review">리뷰</label>
-          <textarea
-            id="review"
-            className={styles.review_text}
-            value={reviewContent}
-            onChange={(e) => setReviewContent(e.target.value)}
-          ></textarea>
-        </div>
-        <div>
-          <label htmlFor="rate">별점</label>
-          <RatingInput value={rate} onChange={setRate} />
-        </div>
-        <button type="submit">작성하기</button>
-      </form>
-    </div>
+      <div className={`${styles.review_common_box} ${styles.review_writer_box}`}>
+        <h1>리뷰 작성하기</h1>
+        <p>닉네임: {user.nickname}</p>
+        <p>이메일: {user.email}</p>
+        <p>Treats ID: {treatsId}</p> {/* 실제 Treats ID 값 표시 */}
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="review">리뷰</label>
+            <textarea
+                id="review"
+                className={styles.review_text}
+                value={reviewContent}
+                onChange={(e) => setReviewContent(e.target.value)}
+            ></textarea>
+          </div>
+          <div>
+            <label htmlFor="rate">별점</label>
+            <RatingInput value={rate} onChange={setRate} />
+          </div>
+          <button type="submit">작성하기</button>
+        </form>
+      </div>
   );
 };
 
