fa029de8e513f5ad70f19029792f4c0714989bf5
hgb926 <hgb926@naver.com>
2024-08-05 20:23:45 +0900

    0805 회원탈퇴 입력값 검증 (패스워드), css까지

--- src/components/auth/user/mypage/DeleteAccountModal.js
+++ src/components/auth/user/mypage/DeleteAccountModal.js
@@ -1,9 +1,17 @@
 import React, {useRef, useState} from 'react';
 import styles from './DeleteAccountModal.module.scss';
+import {debounce} from "lodash";
+import {AUTH_URL} from "../../../../config/user/host-config";
+import {useSelector} from "react-redux";
 
 const DeleteAccountModal = ({ onClose }) => {
 
+
+    const user = useSelector(state => state.userEdit.userDetail);
     const [error, setError] = useState("")
+    const [success, setSuccess] = useState("")
+    const [isValid, setIsValid] = useState(false); // 검증 여부
+    const [isSubmitDisabled, setIsSubmitDisabled] = useState(true); // 비번 일치하면 버튼 활성화
     const modalRef = useRef();
 
     const handleClickOutside = (e) => {
@@ -15,16 +23,52 @@ const DeleteAccountModal = ({ onClose }) => {
     const handleClickInside = () => {
         onClose();
     }
+    const checkPassword = debounce(async (password) => {
+        const response = await fetch(`${AUTH_URL}/check-password/${user.id}?password=${password}`);
+        const flag = await response.json();
+
+        if (flag) {
+            setIsValid(true)
+            setIsSubmitDisabled(false);
+            setSuccess("가지마세요..")
+        } else {
+            setIsValid(false)
+            setIsSubmitDisabled(true);
+            setError("비밀번호가 틀렸습니다.")
+        }
+
+    }, 1000)
+
+    const changeHandler = (e) => {
+        const password = e.target.value;
+        if (password.length === 0) {
+            setIsValid(false)
+            setError('')
+            setSuccess('')
+            setIsSubmitDisabled(false);
+        }
+        checkPassword(password);
+    }
 
     return (
         <div className={styles.overlay} onClick={handleClickOutside}>
             <div className={styles.modal} ref={modalRef}>
                 <h2 className={styles.h2}>정말 탈퇴하시겠습니까?</h2>
                 <p className={styles.p}>탈퇴하시려면 현재 비밀번호를 입력해주세요.</p>
-                <input type="password" placeholder="현재 비밀번호" className={styles.input}/>
-                <p>{error}</p>
+                <input
+                    type="password"
+                    placeholder="현재 비밀번호"
+                    className={styles.input}
+                    onChange={changeHandler}
+                />
+                {isValid && <p className={styles.success}>{success}</p>}
+                {!isValid && <p className={styles.error}>{error}</p>}
                 <div className={styles.flex}>
-                    <button className={styles.confirmButton}>확인</button>
+                    <button
+                        className={styles.confirmButton}
+                        disabled={isSubmitDisabled}
+                    >확인
+                    </button>
                     <button className={styles.confirmButton} onClick={handleClickInside}>취소</button>
                 </div>
             </div>
