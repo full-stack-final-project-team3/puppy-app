9c8e6b7ae87d4df162e44b18e286f2b2aa27430e
hgb926 <hgb926@naver.com>
2024-08-05 15:43:39 +0900

    0805 강아지 중복 렌더링 수정

--- src/pages/user/MyBoards.js
+++ src/pages/user/MyBoards.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import MyPageHeader from "../../components/auth/user/mypage/MyPageHeader";
 import styles from './MyBoards.module.scss'
+import {BOARD_URL} from "../../config/user/host-config";
+import {useSelector} from "react-redux";
 
 const MyBoards = () => {
+
+    const userDetail = useSelector(state => state.userEdit.userDetail);
+    const [data, setData] = useState([]);
+
+    console.log(userDetail)
+
+    // useEffect(() => {
+    //     const fetchList = async () => {
+    //         if (!userDetail.id) return; // userDetail.id가 존재하지 않을 때 방지
+    //
+    //         const response = await fetch(`${BOARD_URL}/boardList/${userDetail.id}`);
+    //         if (response.ok) {
+    //             const result = await response.json();
+    //             setData(result);
+    //         }
+    //     };
+    //
+    //     fetchList();
+    // }, [userDetail.id]);
 
     return (
         <div className={styles.wrap}>
