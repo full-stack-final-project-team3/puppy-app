908c0cfd1f76ca238b047c6031f82f2441a5c3cd
hgb926 <hgb926@naver.com>
2024-08-06 10:41:03 +0900

    0805 마이페이지 my info 스타일

--- src/components/auth/user/mypage/AboutMyInfo.js
+++ src/components/auth/user/mypage/AboutMyInfo.js
@@ -9,35 +9,38 @@ const AboutMyInfo = () => {
 
     return (
         <div className={styles.wrap}>
-            <div className={styles.subWrap}>
-                <Link to={'/hotel-record'} className={styles.suggest}>
-                    <FaHotel className={styles.hotelIcon} />
-                    호텔 예약 내역
-                </Link>
-                <Link to={'/snack-record'} className={styles.suggest}>
-                    <FaBone className={styles.snackIcon} />
-                    간식 구독 내역
-                </Link>
-            </div>
-            <div className={styles.subWrap}>
-                <Link to={'/like-hotel'} className={styles.suggest}>
-                    <FaBookmark className={styles.bookmarkIcon} />
-                    즐겨찾기 한 호텔
-                </Link>
-                <Link to={'/like-snack'} className={styles.suggest}>
-                    <FaStar className={styles.starIcon} />
-                    찜해놓은 간식
-                </Link>
-            </div>
-            <div className={styles.subWrap}>
-                <Link to={'/my-boards'} className={styles.suggest}>
-                    <FaPen className={styles.penIcon} />
-                    내가 쓴 글
-                </Link>
-                <Link to={'/like-boards'} className={styles.suggest}>
-                    <FaHeart className={styles.heartIcon} />
-                    좋아요 누른 글
-                </Link>
+            <div className={styles.secondWrap}>
+                <div className={styles.title}>My Info</div>
+                <div className={styles.subWrap}>
+                    <Link to={'/hotel-record'} className={styles.suggest}>
+                        <FaHotel className={styles.hotelIcon}/>
+                        호텔 예약 내역
+                    </Link>
+                    <Link to={'/snack-record'} className={styles.suggest}>
+                        <FaBone className={styles.snackIcon}/>
+                        간식 구독 내역
+                    </Link>
+                </div>
+                <div className={styles.subWrap}>
+                    <Link to={'/like-hotel'} className={styles.suggest}>
+                        <FaBookmark className={styles.bookmarkIcon}/>
+                        즐겨찾기 한 호텔
+                    </Link>
+                    <Link to={'/like-snack'} className={styles.suggest}>
+                        <FaStar className={styles.starIcon}/>
+                        찜해놓은 간식
+                    </Link>
+                </div>
+                <div className={styles.subWrap}>
+                    <Link to={'/my-boards'} className={styles.suggest}>
+                        <FaPen className={styles.penIcon}/>
+                        내가 쓴 글
+                    </Link>
+                    <Link to={'/like-boards'} className={styles.suggest}>
+                        <FaHeart className={styles.heartIcon}/>
+                        좋아요 누른 글
+                    </Link>
+                </div>
             </div>
         </div>
     );
