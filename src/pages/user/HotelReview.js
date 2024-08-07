31594adb006bccf8a030ed78c9a17a8a4b1e2ce0
hgb926 <hgb926@naver.com>
2024-08-07 20:23:00 +0900

    0807 내 게시물에 남이 댓글달면 알림 남 -> 나 전송

---
+++ src/pages/user/HotelReview.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './HotelReview.module.scss'
+
+const HotelReview = () => {
+    return (
+        <div className={styles.wrap}>
+            HotelReview~
+        </div>
+    );
+};
+
+export default HotelReview;
