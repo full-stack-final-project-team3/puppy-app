b5948d7a5c5562061f63809940c2bf1f65fd63d1
binwoojoo <oooo940212@gmail.com>
2024-08-08 21:53:29 +0900

    장바구니 렌더링4

--- src/pages/shop/CartContent.js
+++ src/pages/shop/CartContent.js
@@ -2,7 +2,15 @@ import React from "react";
 import styles from "./ShowCart.module.scss";
 import { AUTH_URL } from "../../config/user/host-config";
 
-const CartContent = ({ cart, bundles, handleRemoveBundle }) => {
+const CartContent = ({
+  cart,
+  bundles,
+  handleRemoveBundle,
+  handleRemoveCart,
+}) => {
+  const discountedPrice = 69000; // 단일 번들 할인 전 가격
+  const totalDiscountedPrice = discountedPrice * bundles.length; // 할인된 총 가격
+
   return (
     <div className={styles.cartList}>
       {bundles.map((bundle) => (
@@ -11,7 +19,7 @@ const CartContent = ({ cart, bundles, handleRemoveBundle }) => {
             {bundle.dogName}을 위한 맞춤 패키지
           </h4>
           <div className={styles.treatsList}>
-            {bundle.treats.map((treat, treatIndex) => (
+            {bundle.treats.map((treat) => (
               <div key={treat.id} className={styles.treatItem}>
                 {/* 간식 이미지 렌더링 */}
                 {treat.treatsPics.length > 0 && (
@@ -25,8 +33,6 @@ const CartContent = ({ cart, bundles, handleRemoveBundle }) => {
                   />
                 )}
                 <h5 className={styles.itemTitle}>{treat.treatsTitle}</h5>
-                {/* <p>무게: {treat.treatsWeight}g</p> */}
-                {/* 여기에서 간식 수정 기능 추가 가능 */}
               </div>
             ))}
           </div>
@@ -43,9 +49,11 @@ const CartContent = ({ cart, bundles, handleRemoveBundle }) => {
           </div>
         </div>
       ))}
-      {/* 장바구니 비우기 버튼 오른쪽 정렬 */}
       <div className={styles.clearCartContainer}>
-        <button className={styles.clearCartButton} onClick={handleRemoveBundle}>
+        <button
+          className={styles.clearCartButton}
+          onClick={() => handleRemoveCart(cart.id)}
+        >
           장바구니 비우기
         </button>
       </div>
@@ -57,8 +65,16 @@ const CartContent = ({ cart, bundles, handleRemoveBundle }) => {
         </div>
         <div className={styles.totalAmountRow}>
           <span>결제예정금액</span>
-          <span>= {cart.totalPrice}원</span>
+          <span>
+            <span className={styles.totalDisCount}>
+              {totalDiscountedPrice.toLocaleString()}원
+            </span>
+            = {cart.totalPrice.toLocaleString()}원
+          </span>
         </div>
+      </div>
+      <div className={styles.paymentButtonContainer}>
+        <button className={styles.paymentButton}>결제하기</button>
       </div>
     </div>
   );
