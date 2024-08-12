ee1d39d857f131ae4ae12bf08556fca519f09822
binwoojoo <oooo940212@gmail.com>
2024-08-12 21:41:29 +0900

    샵 메인 디자인

--- src/pages/shop/ShopMain.js
+++ src/pages/shop/ShopMain.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./ShopMain.module.scss";
 import { useSelector } from "react-redux";
+import ShopMainBg from "./ShopMainBg";
 
 const ShopMain = () => {
   const [selectedDogId, setSelectedDogId] = useState("");
@@ -50,54 +51,66 @@ const ShopMain = () => {
   };
 
   return (
-    <div className={styles.shopMain}>
-      <div className={styles.content}>
-        <h1>내 강아지 목록</h1>
-        <div className={styles.buttonContainer}>
-          {isLoggedIn &&
-            user.role === "ADMIN" && ( // admin 역할일 때만 상품 추가 버튼 표시
-              <button
-                onClick={handleManagementClick}
-                className={styles.newTreatsBtn}
-              >
-                상품 관리
-              </button>
-            )}
-          {isLoggedIn &&
-            user.role === "ADMIN" && ( // admin 역할일 때만 상품 추가 버튼 표시
-              <button onClick={handleAddClick} className={styles.newTreatsBtn}>
-                상품 추가
-              </button>
-            )}
-        </div>
-        {isLoggedIn ? ( // 로그인 상태일 때
-          dogList.length === 0 ? ( // 강아지가 없는 경우
-            <p>등록된 강아지가 없습니다. 강아지를 등록해 주세요.</p>
-          ) : (
-            <div>
-              <label htmlFor="dogSelect">강아지를 선택하세요:</label>
-              <select
-                id="dogSelect"
-                value={selectedDogId}
-                onChange={handleSelectChange}
-              >
-                <option value="">-- 선택하세요 --</option>
-                {dogList.map((dog) => (
-                  <option key={dog.id} value={dog.id}>
-                    {dog.dogName}
-                  </option>
-                ))}
-              </select>
-            </div>
-          )
-        ) : (
-          <p>로그인을 해주세요.</p> // 로그인하지 않은 경우 안내 메시지
-        )}
-        <button onClick={handleStartClick} className={styles.startButton}>
-          {isLoggedIn ? (dogList.length === 0 ? "등록하기" : "시작") : "로그인"}
-        </button>
+    <>
+      <div className={styles.shopMainContainer}>
+        <ShopMainBg />
       </div>
-    </div>
+      <div className={styles.shopMain}>
+        <div className={styles.content}>
+          <h1>내 강아지 목록</h1>
+          <div className={styles.buttonContainer}>
+            {isLoggedIn &&
+              user.role === "ADMIN" && ( // admin 역할일 때만 상품 추가 버튼 표시
+                <button
+                  onClick={handleManagementClick}
+                  className={styles.newTreatsBtn}
+                >
+                  상품 관리
+                </button>
+              )}
+            {isLoggedIn &&
+              user.role === "ADMIN" && ( // admin 역할일 때만 상품 추가 버튼 표시
+                <button
+                  onClick={handleAddClick}
+                  className={styles.newTreatsBtn}
+                >
+                  상품 추가
+                </button>
+              )}
+          </div>
+          {isLoggedIn ? ( // 로그인 상태일 때
+            dogList.length === 0 ? ( // 강아지가 없는 경우
+              <p>등록된 강아지가 없습니다. 강아지를 등록해 주세요.</p>
+            ) : (
+              <div>
+                <label htmlFor="dogSelect">강아지를 선택하세요:</label>
+                <select
+                  id="dogSelect"
+                  value={selectedDogId}
+                  onChange={handleSelectChange}
+                >
+                  <option value="">-- 선택하세요 --</option>
+                  {dogList.map((dog) => (
+                    <option key={dog.id} value={dog.id}>
+                      {dog.dogName}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            )
+          ) : (
+            <p>로그인을 해주세요.</p> // 로그인하지 않은 경우 안내 메시지
+          )}
+          <button onClick={handleStartClick} className={styles.startButton}>
+            {isLoggedIn
+              ? dogList.length === 0
+                ? "등록하기"
+                : "시작"
+              : "로그인"}
+          </button>
+        </div>
+      </div>
+    </>
   );
 };
 
