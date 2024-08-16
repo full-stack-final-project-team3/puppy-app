259694629c5ce2a6f150c645517b559b328f5e22
hgb926 <hgb926@naver.com>
2024-08-16 21:09:46 +0900

    0816 강아지 알러지 추가 기능 구현

--- src/components/auth/dog/DogEdit.js
+++ src/components/auth/dog/DogEdit.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from './DogEdit.module.scss';
 import { useDispatch, useSelector } from "react-redux";
 import { userEditActions } from "../../store/user/UserEditSlice";
@@ -6,17 +6,20 @@ import { dogEditActions } from "../../store/dog/DogEditSlice";
 import { DOG_URL } from "../../../config/user/host-config";
 import { useNavigate } from "react-router-dom";
 import { decideGender, decideSize, translateAllergy, translateBreed } from './dogUtil.js';
-import { LuBadgeX } from "react-icons/lu";
+import { LuBadgeX, LuPlusCircle } from "react-icons/lu";
 import UserModal from "../user/mypage/UserModal";
-import DogEditSkeleton from "../user/mypage/DogEditSkeleton.js"; // UserModal 컴포넌트 import
+import DogEditSkeleton from "../user/mypage/DogEditSkeleton.js";
 
 const DogEdit = () => {
     const [dogProfileUrl, setDogProfileUrl] = useState('');
     const [showModal, setShowModal] = useState(false);
     const [modalText, setModalText] = useState('');
-    const [pendingFunction, setPendingFunction] = useState(null); // 모달에서 실행할 함수를 저장할 상태
-    const [isDeleteConfirmation, setIsDeleteConfirmation] = useState(false); // 삭제 확인 모드인지 여부
+    const [pendingFunction, setPendingFunction] = useState(null);
+    const [isDeleteConfirmation, setIsDeleteConfirmation] = useState(false);
     const [loading, setLoading] = useState(true);
+    const [showAllergyDropdown, setShowAllergyDropdown] = useState(false);
+    const [selectedAllergies, setSelectedAllergies] = useState([]); // 다중 선택 가능
+
     const weightRef = useRef();
     const fileInputRef = useRef();
 
@@ -25,13 +28,19 @@ const DogEdit = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
+    const allergyOptions = [
+        'BEEF', 'CHICKEN', 'CORN', 'DAIRY', 'FISH', 'FLAX', 'LAMB', 'PORK',
+        'TURKEY', 'WHEAT', 'SOY', 'RICE', 'PEANUT', 'BARLEY', 'OAT', 'POTATO',
+        'TOMATO', 'DUCK', 'SALMON'
+    ];
+
     useEffect(() => {
         window.scrollTo(0, 0);
         const timer = setTimeout(() => {
             setLoading(false);
-        }, 850); // 850ms 후에 loading을 false로 설정
-        return () => clearTimeout(timer); // 컴포넌트 언마운트 시 타이머를 정리
-    }, []); // 컴포넌트가 처음 로딩될 때만 실행
+        }, 850);
+        return () => clearTimeout(timer);
+    }, []);
 
     const handleImageClick = () => {
         fileInputRef.current.click();
@@ -43,12 +52,47 @@ const DogEdit = () => {
             const reader = new FileReader();
             reader.onloadend = () => {
                 setDogProfileUrl(reader.result);
-                const updatedDogList = userDetail.dogList.map(d => d.id === dog.id ? { ...d, dogProfileUrl: reader.result } : d);
-                dispatch(userEditActions.updateUserDetail({ ...userDetail, dogList: updatedDogList }));
-                dispatch(dogEditActions.updateDogInfo({ ...dog, dogProfileUrl: reader.result }));
+                const updatedDogList = userDetail.dogList.map(d => d.id === dog.id ? {
+                    ...d,
+                    dogProfileUrl: reader.result
+                } : d);
+                dispatch(userEditActions.updateUserDetail({...userDetail, dogList: updatedDogList}));
+                dispatch(dogEditActions.updateDogInfo({...dog, dogProfileUrl: reader.result}));
             };
             reader.readAsDataURL(file);
         }
+    };
+
+    const addAllergy = async () => {
+        if (selectedAllergies.length === 0) return;
+
+        const newAllergyList = [...dog.allergies, ...selectedAllergies];
+        const updatedDogList = userDetail.dogList.map(d =>
+            d.id === dog.id ? {...d, allergies: newAllergyList} : d
+        );
+
+        try {
+            const response = await fetch(`${DOG_URL}/allergy`, {
+                method: 'PATCH',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify({allergies: selectedAllergies, dogId: dog.id}),
+            });
+
+            if (response.ok) {
+                dispatch(userEditActions.updateUserDetail({...userDetail, dogList: updatedDogList}));
+                dispatch(dogEditActions.updateDogInfo({...dog, allergies: newAllergyList}));
+                setModalText("알러지가 추가되었습니다!");
+                setPendingFunction(() => {
+                });
+                setShowModal(true);
+            } else {
+                console.error('알러지 추가 실패:', response.statusText);
+            }
+        } catch (error) {
+            console.error('알러지 추가 중 에러 발생:', error);
+        }
+
+        setShowAllergyDropdown(false);
     };
 
     const clearEditMode = async () => {
@@ -60,20 +104,27 @@ const DogEdit = () => {
 
         const response = await fetch(`${DOG_URL}/${dog.id}`, {
             method: "PATCH",
-            headers: { "Content-Type": "application/json" },
+            headers: {"Content-Type": "application/json"},
             body: JSON.stringify(payload),
         });
+
         if (response.ok) {
-            const updatedDogList = userDetail.dogList.map(d => d.id === dog.id ? { ...d, weight, dogProfileUrl: payload.dogProfileUrl } : d);
-            dispatch(userEditActions.updateUserDetail({ ...userDetail, dogList: updatedDogList }));
+            const updatedDogList = userDetail.dogList.map(d => d.id === dog.id ? {
+                ...d,
+                weight,
+                dogProfileUrl: payload.dogProfileUrl
+            } : d);
+            dispatch(userEditActions.updateUserDetail({...userDetail, dogList: updatedDogList}));
             dispatch(userEditActions.clearMode());
             dispatch(dogEditActions.clearEdit());
             setModalText("성공적으로 수정이 완료되었습니다.");
-            setPendingFunction(() => {}); // 모달에서 실행할 함수는 없음
+            setPendingFunction(() => {
+            });
             setShowModal(true);
         } else {
             setModalText("수정에 실패했습니다.");
-            setPendingFunction(() => {}); // 모달에서 실행할 함수는 없음
+            setPendingFunction(() => {
+            });
             setShowModal(true);
         }
     };
@@ -82,26 +133,24 @@ const DogEdit = () => {
         try {
             const response = await fetch(`${DOG_URL}/${dog.id}`, {
                 method: 'DELETE',
-                headers: { 'Content-Type': 'application/json' },
+                headers: {'Content-Type': 'application/json'},
             });
 
             if (response.ok) {
                 const updatedDogList = userDetail.dogList.filter(d => d.id !== dog.id);
-                dispatch(userEditActions.updateUserDetail({ ...userDetail, dogList: updatedDogList }));
+                dispatch(userEditActions.updateUserDetail({...userDetail, dogList: updatedDogList}));
                 dispatch(userEditActions.clearMode());
                 dispatch(dogEditActions.clearEdit());
                 setModalText("삭제 되었습니다.");
-                setPendingFunction(() => navigate("/mypage")); // 모달에서 실행할 함수로 navigate 설정
+                setPendingFunction(() => navigate("/mypage"));
                 setShowModal(true);
             } else {
                 setModalText("삭제에 실패했습니다.");
-                // setPendingFunction(() => {}); // 모달에서 실행할 함수는 없음
                 setShowModal(true);
             }
         } catch (error) {
             console.error('에러 발생:', error);
             setModalText("삭제 중 에러가 발생했습니다.");
-            // setPendingFunction(() => {}); // 모달에서 실행할 함수는 없음
             setShowModal(true);
         }
     };
@@ -112,19 +161,20 @@ const DogEdit = () => {
         try {
             const response = await fetch(`${DOG_URL}/allergy?allergy=${allergy}&dogId=${dog.id}`, {
                 method: 'DELETE',
-                headers: { 'Content-Type': 'application/json' },
+                headers: {'Content-Type': 'application/json'},
             });
 
             if (response.ok) {
                 const updatedAllergies = dog.allergies.filter(a => a !== allergy);
                 const updatedDogList = userDetail.dogList.map(d =>
-                    d.id === dog.id ? { ...d, allergies: updatedAllergies } : d
+                    d.id === dog.id ? {...d, allergies: updatedAllergies} : d
                 );
 
-                dispatch(userEditActions.updateUserDetail({ ...userDetail, dogList: updatedDogList }));
-                dispatch(dogEditActions.updateDogInfo({ ...dog, allergies: updatedAllergies }));
+                dispatch(userEditActions.updateUserDetail({...userDetail, dogList: updatedDogList}));
+                dispatch(dogEditActions.updateDogInfo({...dog, allergies: updatedAllergies}));
                 setModalText("알러지가 삭제되었습니다!");
-                setPendingFunction(() => {}); // 모달에서 실행할 함수는 없음
+                setPendingFunction(() => {
+                });
                 setShowModal(true);
             } else {
                 console.error('알러지 삭제 실패:', response.statusText);
@@ -141,10 +191,10 @@ const DogEdit = () => {
     const handleConfirmModal = () => {
         setShowModal(false);
         if (isDeleteConfirmation) {
-            removeHandler(); // 삭제 확인 모드일 때 removeHandler 실행
+            removeHandler();
         } else {
             if (pendingFunction) {
-                pendingFunction(); // 다른 경우 pendingFunction 실행
+                pendingFunction();
             }
         }
     };
@@ -157,14 +207,22 @@ const DogEdit = () => {
 
     const handleDeleteClick = () => {
         setModalText("정말 삭제하시겠습니까?");
-        setIsDeleteConfirmation(true); // 삭제 확인 모드로 전환
+        setIsDeleteConfirmation(true);
         setShowModal(true);
+    };
+
+    const toggleAllergySelection = (allergy) => {
+        setSelectedAllergies(prevState =>
+            prevState.includes(allergy)
+                ? prevState.filter(a => a !== allergy)
+                : [...prevState, allergy]
+        );
     };
 
     return (
         <div className={styles.wrap}>
             <div className={styles.header}>
-                <img className={styles.img} src="/header-logo.png" alt="Header Logo" />
+                <img className={styles.img} src="/header-logo.png" alt="Header Logo"/>
                 <h2 className={styles.title}>{dog.dogName}의 정보</h2>
             </div>
             <div className={styles.flex}>
@@ -175,7 +233,8 @@ const DogEdit = () => {
                     </div>
                     <div className={styles.row}>
                         <div className={styles.item}>생일</div>
-                        <span className={styles.answer}>{dog.birthday} <span className={styles.sub}>{dog.age}년 {dog.month}개월</span></span>
+                        <span className={styles.answer}>{dog.birthday} <span
+                            className={styles.sub}>{dog.age}년 {dog.month}개월</span></span>
                     </div>
                     <div className={styles.row}>
                         <div className={styles.item}>성별</div>
@@ -183,7 +242,7 @@ const DogEdit = () => {
                     </div>
                     <div className={styles.row}>
                         <div className={styles.item}>체중</div>
-                        <input className={styles.input} placeholder={dog.weight} type="number" ref={weightRef} />
+                        <input className={styles.input} placeholder={dog.weight} type="number" ref={weightRef}/>
                         <span className={styles.sub}>{decideSize(dog.dogSize)}</span>
                     </div>
                     <div className={styles.row}>
@@ -191,27 +250,49 @@ const DogEdit = () => {
                         <div className={styles.answer}>
                             {Array.isArray(dog.allergies) ? dog.allergies.map((allergy, index) => (
                                 <span key={index} className={styles.badge}>
-                                    {translateAllergy(allergy)}
-                                    <LuBadgeX className={styles.icon} onClick={removeAllergy} data-alg={allergy} />
-                                </span>
+                                {translateAllergy(allergy)}
+                                    <LuBadgeX className={styles.icon} onClick={removeAllergy} data-alg={allergy}/>
+                            </span>
                             )) : <span className={styles.answer}>없음</span>}
+                            <LuPlusCircle
+                                className={styles.addIcon}
+                                onClick={() => setShowAllergyDropdown(!showAllergyDropdown)}
+                            />
+                            {showAllergyDropdown && (
+                                <div className={styles.dropdown}>
+                                    {allergyOptions.map((option, index) => (
+                                        <div key={index} className={styles.dropdownItem}>
+                                            <input
+                                                type="checkbox"
+                                                id={`allergy-${option}`}
+                                                checked={selectedAllergies.includes(option)}
+                                                onChange={() => toggleAllergySelection(option)}
+                                            />
+                                            <label htmlFor={`allergy-${option}`}>{translateAllergy(option)}</label>
+                                        </div>
+                                    ))}
+                                    <button onClick={addAllergy} className={styles.addAllergyButton}>추가</button>
+                                </div>
+                            )}
                         </div>
                     </div>
                     <div className={styles.buttons}>
                         <button onClick={handleDeleteClick}>삭제</button>
                         <button onClick={() => {
                             setPendingFunction(() => clearEditMode);
-                            setShowModal(true); // 모달을 먼저 열고, 확인 버튼을 눌렀을 때 clearEditMode 실행
-                        }}>완료</button>
+                            setShowModal(true);
+                        }}>완료
+                        </button>
                     </div>
                 </div>
                 <div className={styles.right} onClick={handleImageClick}>
-                    <img className={styles.dogProfileUrl} src={dog.dogProfileUrl || "header-logo.png"} alt="Dog Profile" />
+                    <img className={styles.dogProfileUrl} src={dog.dogProfileUrl || "header-logo.png"}
+                         alt="Dog Profile"/>
                     <div className={styles.hoverText}>강아지 사진 변경</div>
                     <input
                         type="file"
                         ref={fileInputRef}
-                        style={{ display: 'none' }}
+                        style={{display: 'none'}}
                         onChange={handleFileChange}
                     />
                 </div>
@@ -224,11 +305,11 @@ const DogEdit = () => {
                     onConfirm={handleConfirmModal}
                     onClose={handleCloseModal}
                     confirmButtonText={isDeleteConfirmation ? "예" : "확인"}
-                    showCloseButton={isDeleteConfirmation} // 삭제 확인 모드일 때는 "아니요" 버튼도 표시
+                    showCloseButton={isDeleteConfirmation}
                 />
             )}
         </div>
     );
-};
+}
 
 export default DogEdit;
