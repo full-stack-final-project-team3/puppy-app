fa936ecfa6f26a1f84c6c863d5402ff8f421975d
jzinit <kyj32323@naver.com>
2024-08-16 15:25:40 +0900

    수정

--- src/pages/shop/order/OrderDetail.js
+++ src/pages/shop/order/OrderDetail.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom'; 
+import { useSelector } from 'react-redux';
 import styles from './scss/OrderDetail.module.scss';
-import { useDispatch, useSelector } from 'react-redux';
 
 const subscriptionPeriodLabels = {
     ONE: "1개월",
@@ -9,36 +9,81 @@ const subscriptionPeriodLabels = {
     MONTH6: "6개월",
 };
 
+// 구독 만료 기간을 계산하는 함수
+const calculateExpiryDate = (startDate, subscriptionPeriod) => {
+    const expiryDate = new Date(startDate);
+    const monthsToAdd = {
+        ONE: 1,
+        MONTH3: 3,
+        MONTH6: 6,
+    };
+    expiryDate.setMonth(expiryDate.getMonth() + monthsToAdd[subscriptionPeriod]);
+    return expiryDate.toLocaleDateString('ko-KR', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+    });
+};
+
+const formatDateTime = (dateTimeString) => {
+    const options = {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        hour12: false
+    };
+    return new Date(dateTimeString).toLocaleString('ko-KR', options);
+};
+
 const OrderDetail = () => {
     const user = useSelector((state) => state.userEdit.userDetail);
     const location = useLocation();
-    const { order } = location.state; // 전달된 order 데이터를 받아옴
+    const order = location.state?.order; // 전달된 order 데이터를 받아옴, 없을 경우 undefined
+
+    if (!order) {
+        return <div className={styles.wrap}><p>주문 정보를 불러올 수 없습니다.</p></div>;
+    }
 
     return (
         <div className={styles.wrap}>
-            <h1>주문 상세 내역</h1>
-            <div className={styles.orderInfo}>
-                <p><strong>이름: {user.nickname}</strong> </p>
-                <p><strong>이메일: {user.email}</strong> </p>
-                <p><strong>핸드폰 번호: {user.phoneNumber}</strong></p>
-                <p><strong>주소: {order.address || 'N/A'}</strong></p>
-            </div>
-            {order.bundles.map((bundle, index) => (
-                <div key={index} className={styles.bundleItem}>
-                    <p><strong>상품명 :</strong> 반려견 전용 맞춤형 푸드 패키지 For {bundle.dogName}</p>
-                    <p><strong>상품 구독 기간 :</strong> 
-                        {subscriptionPeriodLabels[order.subscriptionPeriods[bundle.id]] || '구독 기간 정보 없음'}
-                    </p>
-                    <p><strong>패키지 리스트:</strong></p>
-                    <ul>
-                        {bundle.treats.map((treat, treatIndex) => (
-                            <li key={treatIndex}>{treat.treatsTitle}</li>
-                        ))}
-                    </ul>
+            <h1 className={styles.title}>주문 상세 내역</h1>
+            <div className={styles.orderDetail}>
+                <div className={styles.userInfo}>
+                    <h2>주문자 정보</h2>
+                    <p><strong>이름:</strong> {user?.nickname || '에러'}</p>
+                    <p><strong>이메일:</strong> {user?.email || '에러'}</p>
+                    <p><strong>핸드폰 번호:</strong> {user?.phoneNumber || '에러'}</p>
+                    <p><strong>주소:</strong> {user?.address || '에러'}</p>
                 </div>
-            ))}
-            <div className={styles.totalPrice}>
-                <p><strong>총 금액:</strong> {order.totalPrice ? order.totalPrice.toLocaleString() : 'N/A'}원</p>
+                <div className={styles.orderInfo}>
+                    <h2>주문 정보</h2>
+                    <p><strong>주문 날짜:</strong> {formatDateTime(order.orderDateTime) || '에러'}</p>
+                    <p><strong>배송 요청 사항:</strong> {order.deliveryRequest || '없음'}</p>
+                </div>
+                <div className={styles.bundles}>
+                    <h2>주문 상품</h2>
+                    {order.bundles?.map((bundle, index) => (
+                        <div key={index} className={styles.bundleItem}>
+                            <h3>반려견 전용 맞춤형 푸드 패키지 For {bundle.dogName}</h3>
+                            <p><strong>상품 구독 기간:</strong> {subscriptionPeriodLabels[bundle.subsType] || '구독 기간 정보 없음'}</p>
+                            <p><strong>구독 만료 기간:</strong> 
+                                {calculateExpiryDate(order.orderDateTime, bundle.subsType)}
+                            </p>
+                            <p><strong>패키지 리스트:</strong></p>
+                            <ul>
+                                {bundle.treats?.map((treat, treatIndex) => (
+                                    <li key={treatIndex}>{treat.treatTitle}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
+                </div>
+                <div className={styles.totalPrice}>
+                    <h2>총 금액</h2>
+                    <p>{order.totalPrice ? order.totalPrice.toLocaleString() : '에러'} 원</p>
+                </div>
             </div>
         </div>
     );
--- src/pages/shop/order/OrderInfo.js
+++ src/pages/shop/order/OrderInfo.js
@@ -1,86 +1,104 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './scss/OrderPage.module.scss';
+import EditInfoModal from './EditInfoModal';
+import EditUserInfoModal from './EditUserInfoModal';
 
 const OrderInfo = ({
   user,
   orderInfo,
-  handlePhoneNumberChange,
-  handleAddressChange,
-  handleAddressSearch,
-}) => (
-  <div>
-    <div className={styles.section}>
-      <div className={styles['section-title']}>구매자 정보</div>
-      <div className={styles['section-content']}>
-        <p>이름: {user.nickname}</p>
-        <p>이메일: {user.email}</p>
-        <div>
-          <label>휴대폰 번호: </label>
-          {user.phoneNumber ? (
-            <p>{user.phoneNumber}</p>
-          ) : (
-            <input
-              type="text"
-              value={orderInfo.receiverPhone}
-              onChange={handlePhoneNumberChange}
-              placeholder="휴대폰 번호 입력"
-              required
-            />
-          )}
+  handleReceiverInfoUpdate,
+  handleUserInfoUpdate,
+  handleDeliveryRequestChange,  // 부모 컴포넌트로부터 전달받은 함수
+  handleCustomRequestChange,
+}) => {
+  const [showModal, setShowModal] = useState(false);
+  const [editingType, setEditingType] = useState(null); // 모달창의 용도 구분
+
+  const handleSave = (newName, newPhone, address, detailAddress) => {
+    if (editingType === 'user') {
+      handleUserInfoUpdate(newName, newPhone); // 구매자 정보 수정
+    } else if (editingType === 'receiver') {
+      handleReceiverInfoUpdate(newName, newPhone, address, detailAddress); // 받는 사람 정보 수정
+    }
+    setShowModal(false); // 모달 닫기
+  };
+
+  return (
+    <div>
+      <div className={styles.section}>
+        <div className={styles['section-title']}>
+          구매자 정보
+          <button onClick={() => { setShowModal(true); setEditingType('user'); }}>
+            구매자 정보 수정
+          </button>
+        </div>
+        <div className={styles['section-content']}>
+          <p>이름: {user.realName || '정보 없음'}</p>
+          <p>이메일: {user.email || '정보 없음'}</p>
+          <p>연락처: {user.phoneNumber || '정보 없음'}</p>
         </div>
       </div>
-    </div>
 
-    <div className={styles.section}>
-      <div className={styles['section-title']}>받는 사람 정보</div>
-      <div className={styles['section-content']}>
-        <p>이름: {orderInfo.receiverName}</p>
-        <div>
-          <label>휴대폰 번호: </label>
-          {user.phoneNumber ? (
-            <p>{user.phoneNumber}</p>
-          ) : (
-            <input
-              type="text"
-              value={orderInfo.receiverPhone}
-              onChange={handlePhoneNumberChange}
-              placeholder="휴대폰 번호 입력"
-              required
-            />
-          )}
+      <div className={styles.section}>
+        <div className={styles['section-title']}>
+          받는 사람 정보
+          <button onClick={() => { setShowModal(true); setEditingType('receiver'); }}>
+            받는 사람 정보 수정
+          </button>
         </div>
-        <div>
-          <input
-            type="text"
-            name="location"
-            placeholder="Location"
-            value={orderInfo.receiverAddress}
-            onChange={handleAddressChange}
+        <div className={styles['section-content']}>
+          <p>이름: {orderInfo.receiverName || '정보 없음'}</p>
+          <p>연락처: {orderInfo.receiverPhone || '정보 없음'}</p>
+          <p>주소: {orderInfo.receiverAddress || '정보 없음'}</p>
+          <p>상세 주소: {orderInfo.receiverDetailAddress || user.detailAddress}</p> {/* 상세 주소 출력 추가 */}
+          <p>배송 요청 사항:</p>
+          <select
+            value={orderInfo.deliveryRequest}
+            onChange={handleDeliveryRequestChange}  // 이곳에서 handleDeliveryRequestChange 함수를 사용
             required
-            disabled
-          />
-          <button onClick={handleAddressSearch}>주소 검색</button>
-        </div>
-        <div>
-          <input placeholder='나머지 주소를 입력해' />
-        </div>
-      </div>
-    </div>
+          >
+            <option value="문앞">문 앞</option>
+            <option value="직접">직접 받고 부재 시 문 앞</option>
+            <option value="경비실">경비실</option>
+            <option value="택배함">택배함</option>
+            <option value="기타사항">기타사항</option>
+          </select>
 
-    <div className={styles.section}>
-      <div className={styles['section-title']}>배송 정보</div>
-      <div className={styles['section-content']}>
-        <p>배송 요청 사항:</p>
-        <select>
-          <option value="문 앞">문 앞</option>
-          <option value="직접 받고 부재 시 문 앞">직접 받고 부재 시 문 앞</option>
-          <option value="경비실">경비실</option>
-          <option value="택배함">택배함</option>
-          <option value="기타사항">기타사항</option>
-        </select>
+          {orderInfo.deliveryRequest === '기타사항' && (
+            <div className={styles.customRequest}>
+              <input
+                type="text"
+                placeholder="기타 요청 사항을 입력하세요"
+                value={orderInfo.customRequest}
+                onChange={handleCustomRequestChange}
+                required
+              />
+            </div>
+          )}
+        </div>
       </div>
+
+      {showModal && editingType === 'user' && (
+        <EditUserInfoModal
+          name={user.realName}
+          phone={user.phoneNumber}
+          onSave={handleSave}
+          onClose={() => setShowModal(false)}
+        />
+      )}
+
+      {showModal && editingType === 'receiver' && (
+        <EditInfoModal
+          receiverName={orderInfo.receiverName}
+          receiverPhone={orderInfo.receiverPhone}
+          receiverAddress={orderInfo.receiverAddress}
+          receiverDetailAddress={orderInfo.receiverDetailAddress} // 상세 주소를 넘겨줍니다
+          onSave={handleSave}
+          onClose={() => setShowModal(false)}
+        />
+      )}
     </div>
-  </div>
-);
+  );
+};
 
 export default OrderInfo;
