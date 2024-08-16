2f07426bc1c55636a0a6aaf71c39ab6604187c2e
chan <chan9505@naver.com>
2024-08-16 19:13:59 +0900

    # StepIndicator 수정

--- src/components/auth/user/signup/StepIndicator.js
+++ src/components/auth/user/signup/StepIndicator.js
@@ -2,16 +2,14 @@ import React from 'react';
 import styles from './StepIndicator.module.scss';
 
 const StepIndicator = ({ step, onStepClick }) => {
-  const stepLabels = ['Email', 'Nickname Password', 'Address PhoneNumber', 'Add Dog'];
+  const stepLabels = ['Email', 'Nickname / Password', 'Address / PhoneNumber', 'Welcome'];
 
   return (
     <div className={styles.stepIndicator}>
       {[1, 2, 3, 4].map((num, idx) => (
         <React.Fragment key={num}>
           {idx > 0 && <div className={`${styles.stepLine} ${step > num - 1 ? styles.active : ''}`} />}
-          <di1 v
-            className={`${styles.stepContainer}`}
-          >
+          <div className={`${styles.stepContainer}`}>
             <div
               className={`${styles.stepNumber} ${step >= num ? styles.active : ''}`}
               onClick={num < step ? () => onStepClick(num) : null}
@@ -20,7 +18,7 @@ const StepIndicator = ({ step, onStepClick }) => {
               {num}
             </div>
             <div className={styles.stepLabel}>{stepLabels[num - 1]}</div>
-          </di1>
+          </div>
         </React.Fragment>
       ))}
     </div>
