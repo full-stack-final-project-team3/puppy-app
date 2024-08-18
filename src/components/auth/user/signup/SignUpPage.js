f2d7c361da8f80e971669d43052f2dfbeb261c41
chan <chan9505@naver.com>
2024-08-19 00:11:49 +0900

    # SignUpPage 수정

--- src/components/auth/user/signup/SignUpPage.js
+++ src/components/auth/user/signup/SignUpPage.js
@@ -239,6 +239,7 @@ const SignUpPage = () => {
                 <WelcomePage onAddDogClick={autoLoginHandler} />
               )}
             </div>
+            </div>
           </form>
         </CSSTransition>
       </>
