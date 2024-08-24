d0c99720bb7d8c33ad47914fc2600980417dd9b5
kkimjuseong <rofodzl1@naver.com>
2024-08-25 00:02:09 +0900

    # 호스트 컨피그 front_url 추가

--- src/config/user/host-config.js
+++ src/config/user/host-config.js
@@ -1,16 +1,21 @@
 const LOCAL_PORT = 8888; // 백엔드 로컬 서버 포트번호
+const LOCAL_FRONT = 3000;
 
 const clientHostName = window.location.hostname;
 
 let backendHostName;
+let frontendHostName;
 
 if (clientHostName === "localhost") {
   backendHostName = "http://localhost:" + LOCAL_PORT;
+  frontendHostName = "http://localhost:" + LOCAL_FRONT;
 } else {
   backendHostName = 'http://43.203.105.27:8888';
+  frontendHostName = "http://43.203.105.27:3000";
 }
 
 const API_BASE_URL = backendHostName;
+const APP_BASE_URL = frontendHostName;
 
 const EVENT = "/events";
 const HOTEL = "/hotel";
@@ -48,5 +53,6 @@ export const REVIEW_URL = API_BASE_URL + REVIEW;
 export const LIKE_URL = API_BASE_URL + likes;
 export const RESERVATION_URL = API_BASE_URL + RESERVATION;
 export const HOTEL_REVIEW_URL = API_BASE_URL + HOTEL_REVIEW;
+export const FRONT = APP_BASE_URL;
 
 export const RESOURCES_URL = API_BASE_URL;
